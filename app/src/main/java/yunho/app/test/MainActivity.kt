c748a2d5c70dd94583d3c36e29855a97dcd54b93
Ju-lotty <jjg991201@gmail.com>
2022-08-20 02:05:10 +0900

    냥!

--- app/src/main/java/yunho/app/test/MainActivity.kt
+++ app/src/main/java/yunho/app/test/MainActivity.kt
@@ -2,10 +2,13 @@ package yunho.app.test
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Toast.makeText(this,"Push 확인!", Toast.LENGTH_LONG).show()
+        //되는 거냐구~
     }
 }
